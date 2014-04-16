b0eefd93a8d92227e999a06c27240d50ff9dc76b
Enzhaev Ivan <8observer8@gmail.com>
2014-04-16 16:29:02 +0400

    In processing of writing instruction.

---
+++ myFirstWebSite.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>My First Website</title>
+    </head>
+    <body>
+        <?php
+        require 'menu.inc.php';
+        require 'libs.inc.php';
+        ?>
+
+        <h1>My First Website</h1>
+        
+        <ul>
+            <li>create project by step-by-step instruction: <a href="jsMyFirstProject.php">My First Project in JavaSript</a></li>
+            <li>go to the free hosting and create account: <a href="http://freeoda.com/">http://freeoda.com/</a></li>
+            <li>download and install "Total Commander": <a href="http://www.ghisler.com/download.htm">http://www.ghisler.com/download.htm</a></li>
+            <li>run "Total Commander"</li>
+            <li></li>
+        </ul>
+    </body>
+</html>
