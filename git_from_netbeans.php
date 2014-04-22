9ae8b3c7ba885bcaed9db2ee07d5c11ca276dfa8
Enzhaev Ivan <8observer8@gmail.com>
2014-04-22 21:17:56 +0400

    Added git_from_netbeans

---
+++ git_from_netbeans.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Saving Project on Git repository from NetBeans</title>
+    </head>
+    <body>
+        <?php
+        require 'libs.inc.php';
+        require 'menu.inc.php';
+        ?>
+
+        <h1>Saving Project on Git repository from NetBeans</h1>
+    </body>
+</html>
