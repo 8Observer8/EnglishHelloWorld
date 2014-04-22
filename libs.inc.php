03fa97f29f34c3c4cc3694169a1e12b37664db0b
Enzhaev Ivan <8observer8@gmail.com>
2014-04-22 09:15:55 +0400

    Added themes.

--- libs.inc.php
+++ libs.inc.php
@@ -1,7 +1,9 @@
 <script type="text/javascript" src="js/libs/jquery-1.9.0/jquery.min.js"></script>
 <script type="text/javascript" src="js/libs/jqueryui-1.10.0/jquery-ui.min.js"></script>
-<link rel="stylesheet" type="text/css" href="css/jquery.ui.theme.css"/>
-<link rel="stylesheet" type="text/css" href="css/jquery-ui.css"/>
-<link rel="stylesheet" type="text/css" href="css/jquery-ui.min.css"/>
+
+<link rel="stylesheet" type="text/css" href="css/themes/sunny/jquery.ui.theme.css"/>
+<link rel="stylesheet" type="text/css" href="css/themes/sunny/jquery-ui.css"/>
+<link rel="stylesheet" type="text/css" href="css/themes/sunny/jquery-ui.min.css"/>
+
 <script type="text/javascript" src="js/script.js"></script>
 <link rel="stylesheet" type="text/css" href="css/stylesheet.css"/>
